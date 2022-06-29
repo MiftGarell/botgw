c536475f6b68ee46107933bf308f44e3fb1d97cb
MiftGarell <108374303+MiftGarell@users.noreply.github.com>
2022-06-29 13:39:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,8 +13,8 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.linkGC = ['https://chat.whatsapp.com/EVCGfzxLWfp81n0WhmVklH', 'https://chat.whatsapp.com/FnNAbem8o6r4pgLhSdO8Q9', 'https://chat.whatsapp.com/HBhy7rVae3o0PkIfkhvs2N'] // ganti jadi group lu
-global.owner = ['6285813385281', '62858929626673', '6285892962667'] // Masukan nomot kalian
+global.linkGC = ['https://chat.whatsapp.com/F5e9ids9afVDLPV3BMvtFb', 'https://chat.whatsapp.com/Fq4f7obd3l16nlezxDgN3S', 'https://chat.whatsapp.com/HBhy7rVae3o0PkIfkhvs2N'] // ganti jadi group lu
+global.owner = ['6288217066498', '6288217066498', '6288217066498'] // Masukan nomot kalian
 global.mods = ['6289669349047', '6288261394557'] // Moderator
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
